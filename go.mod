eefa94a9f660b19c55298e9a69198cbcc55239a0
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2019-04-19 00:20:14 -0500

    Basic challenge server (responder)

---
+++ go.mod
@@ -0,0 +1,7 @@
+module acme_challenge_server
+
+require (
+	github.com/rohanthewiz/element v0.1.2
+	github.com/rohanthewiz/rxrouter v0.0.0-20190127073143-ee2117daa705
+	github.com/valyala/fasthttp v1.2.0
+)
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"github.com/rohanthewiz/element"
+	"github.com/rohanthewiz/rxrouter"
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	rx := rxrouter.New(rxrouter.Options{
+		Verbose: true,
+		TLS: rxrouter.RxTLS{
+			UseTLS:   false,
+			CertFile: "/etc/letsencrypt/live/mysite.com/cert.pem",
+			KeyFile:  "/etc/letsencrypt/live/mysite.com/privkey.pem",
+		},
+	})
+	// Env port override
+	prt := os.Getenv("RX_PORT")
+	if prt != "" {
+		rx.Options.Port = prt
+	}
+
+	tls := strings.ToLower(os.Getenv("RX_USE_TLS"))
+	if tls != "" && tls != "false" && tls != "no" {
+		rx.Options.TLS.UseTLS = true
+		go nonsecureToSecureRedirector()
+	}
+
+	rx.Use( // Logging middleware
+		func(ctx *fasthttp.RequestCtx) (ok bool) {
+			log.Printf("Requested path: %s", ctx.Path())
+			return true
+		},
+		fasthttp.StatusServiceUnavailable, // 503
+	)
+
+	// Auth middleware (example)
+	rx.Use(
+		func(ctx *fasthttp.RequestCtx) (ok bool) {
+			authed := true // pretend we got a good response from our auth check
+			if !authed {
+				return false
+			}
+			return true
+		},
+		fasthttp.StatusUnauthorized,
+	)
+
+	// Add some routes
+	rx.AddRoute("/", handleRoot)
+	rx.AddRoute("/hello/:name/:age", func(ctx *fasthttp.RequestCtx, params map[string]string) {
+		ctx.WriteString(fmt.Sprintf("Hello %s. You are %s!", params["name"], params["age"]))
+	})
+	// Routes for static files
+	rx.AddStaticFilesRoute("/images/", "./assets/images", 1)
+	rx.AddStaticFilesRoute("/css/", "./assets/css", 1)
+	rx.AddStaticFilesRoute("/.well-known/acme-challenge/", "./certs", 0)
+
+	// Let it rip!
+	rx.Start()
+}
+
+func handleRoot(ctx *fasthttp.RequestCtx, params map[string]string) {
+	e := element.New
+	htm := "<!DOCTYPE html>" +
+		e("html").R(
+			e("head").R(
+				e("title").R("Under Maintenance"),
+			),
+			e("body").R(
+				e("div", "class", "main-content").R(
+					e("h2").R("This site is currently under maintenance. Please check back later, and thanks for your patience"),
+				),
+			),
+		)
+	ctx.SetContentType("text/html; charset=utf-8")
+	ctx.WriteString(htm)
+}
+
+func nonsecureToSecureRedirector() {
+	fx := rxrouter.New(rxrouter.Options{
+		Verbose: true,
+		Port:    "80",
+		TLS: rxrouter.RxTLS{
+			UseTLS: false, // just being explicit here
+		},
+	})
+
+	fx.AddCustomMasterHandler( // TODO - replace mysite with your site below
+		func(ctx *fasthttp.RequestCtx) { ctx.Redirect("https://mysite.com", fasthttp.StatusTemporaryRedirect) })
+
+	fx.Start()
+}
